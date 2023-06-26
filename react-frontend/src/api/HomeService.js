5dced78ae69a7e8d2c02729da97cf8c7250f399e
Stefan-ys <84866588+Stefan-ys@users.noreply.github.com>
2023-06-26 23:03:51 +0300

    Add files via upload

--- react-frontend/src/api/HomeService.js
+++ react-frontend/src/api/HomeService.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 
 const HomeService = () => {
+
     try {
-        return axios.get("http://localhost:8080/");
+        return axios.get("http://localhost:8080/").then(response => {
+            return response.data
+        });
     } catch (err) {
         let error = "";
         if (err.response) {
             error += err.response;
         }
+        console.log(error);
         return error;
     }
 };
--- react-frontend/src/components/root/home/Home.jsx
+++ react-frontend/src/components/root/home/Home.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import HomeService from "../../../api/HomeService";
+
 
 function Home() {
+
+    const [welcomeMessage, setWelcomeMessage] = useState("");
+
+    useEffect(() => {
+        HomeService()
+            .then((response) => {
+                setWelcomeMessage(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
             <p>content content content content</p>
+            <p>{welcomeMessage}</p>
         </div>
     );
 }
