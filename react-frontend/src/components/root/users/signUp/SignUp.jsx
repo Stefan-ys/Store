b6b583a7b7b62b5b2529e2e449ae8ae6ec7567c0
Stefan-ys <84866588+Stefan-ys@users.noreply.github.com>
2023-07-02 23:02:37 +0300

    Add files via upload

--- react-frontend/src/components/root/users/signUp/SignUp.jsx
+++ react-frontend/src/components/root/users/signUp/SignUp.jsx
@@ -1,110 +1,113 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
+import {faCheck, faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{4,20}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 
 const SignUpForm = () => {
-        const [formData, setFormData] = useState({
-            username: "",
-            email: "",
-            password: "",
-            confirmPassword: "",
-        });
+    const userRef = useRef();
+    const errRef = useRef();
 
-        const [errors, setErrors] = useState({});
+    const [user, setUser] = useState("");
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
 
-        const handleChange = (e) => {
-            setFormData({
-                ...formData,
-                [e.target.name]: e.target.value,
-            });
-        };
+    const [password, setPassword] = useState("");
+    const [validPassword, setValidPassword] = useState(false);
+    const [passwordFocus, setPasswordFocus] = useState(false);
 
-        const handleSubmit = async (event) => {
-            event.preventDefault();
+    const [matchPassword, setMatchPassword] = useState("");
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
 
-            const validationErrors = {};
-            if (formData.username.trim() === "") {
-                validationErrors.username = "Username is required";
-            }
-            if (formData.email.trim() === "") {
-                validationErrors.email = "Email is required";
-            }
-            if (formData.password.trim() === "") {
-                validationErrors.password = "Password is required";
-            }
-            if (formData.confirmPassword.trim() === "") {
-                validationErrors.confirmPassword = "Confirmation password is required";
-            }
+    const [errMsg, setErrMsg] = useState("");
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user);
+        console.log(result);
+        console.log(user);
+        setValidName(result);
+    }, [user])
+
+    useEffect(() => {
+        const result = PASSWORD_REGEX.test(password);
+        console.log(result);
+        console.log(password);
+        setValidPassword(result);
+        const match = password === matchPassword;
+        setValidMatch(match);
+    }, [password, matchPassword])
+
+    useEffect(() => {
+        setErrMsg("");
+    }, [user, password, matchPassword])
 
 
-            if (Object.keys(validationErrors).length > 0) {
-                setErrors(validationErrors);
-            } else {
-                console.log("Form submitted successfully");
-            }
+    return (
+        <section>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>Register</h1>
+            <form>
+                <label htmlFor="username">
+                    Username:
+                    <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"}/>
+                    <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"}/>
+                </label>
+                <input
+                    type="text"
+                    id="username"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required
+                    aria-invalid={validName ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}
+                />
+                <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle}/>
+                    4 to 24 characters.<br/>
+                    Must begin with a letter.<br/>
+                    Letters, numbers, underscores, hyphens allowed.
+                </p>
 
-            setFormData({
-                username: "",
-                email: "",
-                password: "",
-                confirmPassword: "",
-            });
-        };
+                <label htmlFor="password">
+                    Username:
+                    <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"}/>
+                    <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"}/>
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    ref={userRef}
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required
+                    aria-invalid={validName ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}
+                />
+                <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle}/>
+                    4 to 24 characters.<br/>
+                    Must begin with a letter.<br/>
+                    Letters, numbers, underscores, hyphens allowed.
+                </p>
 
-        return (
 
-            <div>
-                <h1>Signup</h1>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label>Username</label>
-                        <input
-                            type="text"
-                            name="username"
-                            value={formData.username}
-                            onChange={handleChange}
-                            required
-                        />
-                        <br/>
-                    </div>
-                    <div>
-                        <label>Email</label>
-                        <input
-                            type="text"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            required
-                        />
-                        <br/>
-                    </div>
-                    <div>
-                        <label>Password</label>
-                        <input
-                            type="text"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            required
-                        />
-                        <br/>
-                    </div>
-                    <div>
-                        <label>Confirm Password</label>
-                        <input
-                            type="text"
-                            name="confirmPassword"
-                            value={formData.confirmPassword}
-                            onChange={handleChange}
-                            required
-                        />
-                        <br/>
-                    </div>
+            </form>
+        </section>
+    )
 
-                    <button type="submit">Signup</button>
-                </form>
-            </div>
-        );
-
-    }
-;
+}
 export default SignUpForm;
