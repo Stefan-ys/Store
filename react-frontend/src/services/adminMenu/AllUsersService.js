a489462ca28ec74a613cf2fc3cd4460ea268f476
Stefan-ys <84866588+Stefan-ys@users.noreply.github.com>
2023-07-26 23:49:03 +0300

    Add files via upload

--- react-frontend/src/services/adminMenu/AllUsersService.js
+++ react-frontend/src/services/adminMenu/AllUsersService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const AllUsersService = async () => {
     try {
-        const response = await axios.get("http://localhost:8080/users/all");
+        const response = await axios.get("http://localhost:8080/api/users/all");
         console.log(response.data);
         return response.data;
     } catch (error) {
