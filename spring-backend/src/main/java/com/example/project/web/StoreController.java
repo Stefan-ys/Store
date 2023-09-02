07c1e0de0f8906abd999a0b311090450f6af75a4
Stefan-ys <84866588+Stefan-ys@users.noreply.github.com>
2023-09-02 20:37:02 +0300

    Add files via upload

--- spring-backend/src/main/java/com/example/project/web/StoreController.java
+++ spring-backend/src/main/java/com/example/project/web/StoreController.java
@@ -2,16 +2,17 @@ package com.example.project.web;
 
 import com.example.project.payload.response.ProductResponse;
 import com.example.project.service.StoreService;
+import com.example.project.utils.BigDecimalComparator;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,26 +23,34 @@ import java.util.Map;
 @RequestMapping("/api/store")
 public class StoreController {
     private final StoreService storeService;
+
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/all-products")
     public ResponseEntity<Map<String, Object>> getProductsPage(
-            @RequestParam(required = false) String status,
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "3") int size
+            @RequestParam(defaultValue = "3") int size,
+            @RequestParam(defaultValue = "all") String category,
+            @RequestParam(defaultValue = "date") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortOrder
     ) {
-
         try {
-            List<ProductResponse> products = new ArrayList<>();
-            Pageable paging = PageRequest.of(page, size);
+            Sort.Direction direction = sortOrder.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+            String property = switch (sortBy) {
+                case "price" -> "price";
+                case "name" -> "name";
+                default -> "createdDate";
+            };
+
+            Pageable paging = PageRequest.of(page, size, direction, property);
 
             Page<ProductResponse> productsPage;
-            if (status == null) {
+            if (category == null || category.equalsIgnoreCase("all")) {
                 productsPage = storeService.getAllProducts(paging);
             } else {
-                productsPage = storeService.getAllProductsByStatus(status, paging);
+                productsPage = storeService.getProductsByCategory(category, paging);
             }
 
-            products = productsPage.getContent();
+            List<ProductResponse> products = productsPage.getContent();
             Map<String, Object> response = new HashMap<>();
             response.put("products", products);
             response.put("currentPage", productsPage.getNumber());
@@ -49,11 +58,9 @@ public class StoreController {
             response.put("totalPages", productsPage.getTotalPages());
 
             return new ResponseEntity<>(response, HttpStatus.OK);
-
-
-
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
 }
