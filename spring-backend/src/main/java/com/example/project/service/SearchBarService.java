eceba16cfd1c3538d7d76fd68e2db97648f6d837
Stefan-ys <84866588+Stefan-ys@users.noreply.github.com>
2023-10-24 22:46:22 +0300

    Delete spring-backend/src/main/java/com/example/project/service/SearchBarService.java

--- spring-backend/src/main/java/com/example/project/service/SearchBarService.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.service;
-
-import com.example.project.payload.response.ProductResponse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface SearchBarService {
-    Page<ProductResponse> searchForProduct(Pageable paging, String keyWord);
-}
